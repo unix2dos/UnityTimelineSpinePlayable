edbd14e81dbb87f52446b77ce3cde8c581cf9e15
liuwei <levonfly@gmail.com>
2017-10-25 14:06:10 +0800

    update

--- Assets/Scripts/Playables/Spine/SpineAsset.cs
+++ Assets/Scripts/Playables/Spine/SpineAsset.cs
@@ -11,26 +11,33 @@ using UnityEngine.Timeline;
 public class SpineAsset : PlayableAsset, ITimelineClipAsset
 {
     public ExposedReference<GameObject> spineObject;
-    [ContextMenuItem ("ActionNames", "getActionNames")]
+
+    [SpineAnimation(dataField:"_skeletonDataAsset")]
     public string actionName;
+
     [ContextMenuItem ("ActionDuration", "getActionDuration")]
-    public bool actionHold = false;//播放完毕是否保持
+    public bool actionKeep = false;//播放完毕是否保持
+
 
     //spine对象
     private SkeletonAnimation _spine = null;
+    [HideInInspector]
+    public SkeletonDataAsset _skeletonDataAsset;
 
 
-    private void getActionNames ()
-    {
-        if (_spine != null &&  _spine.AnimationState != null) {
-            string strNames = "";
-            ExposedList<Spine.Animation> animations = _spine.AnimationState.Data.skeletonData.animations;
-            for (int i = 0, n = animations.Count; i < n; i++) {
-                strNames = strNames + animations.Items [i].name + "  ";
-            }
-            Debug.Log ("Actions:--->   " + strNames);
-        }
-    }
+//    [ContextMenuItem ("ActionNames", "getActionNames")]
+//    public string actionName;
+//    private void getActionNames ()
+//    {
+//        if (_spine != null &&  _spine.AnimationState != null) {
+//            string strNames = "";
+//            ExposedList<Spine.Animation> animations = _spine.AnimationState.Data.skeletonData.animations;
+//            for (int i = 0, n = animations.Count; i < n; i++) {
+//                strNames = strNames + animations.Items [i].name + "  ";
+//            }
+//            Debug.Log ("Actions:--->   " + strNames);
+//        }
+//    }
 
     private void getActionDuration ()
     {
@@ -45,12 +52,13 @@ public class SpineAsset : PlayableAsset, ITimelineClipAsset
     {
         var playable = ScriptPlayable<SpinePlayable>.Create (graph);
         var spine = spineObject.Resolve (graph.GetResolver ());
-        playable.GetBehaviour ().Initialize (spine, actionName, actionHold);
 //        var abc = owner.GetComponent<PlayableDirector> ();
         if (spine != null)
         {
             _spine = spine.GetComponent<SkeletonAnimation> ();
+            _skeletonDataAsset = _spine.SkeletonDataAsset;
         }
+        playable.GetBehaviour ().Initialize (spine, actionName, actionKeep);
         return playable;
     }
 
